fe3644e6725277396bc50058d219a9a8e10a6466
nam long <namlongyonko02062003@gmail.com>
2024-09-13 22:46:53 -0800

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with HoiDanIT!";
+        return "Hello World with IT ";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can see this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can see this page";
     }
 }
